adbd95e8c4fbd29429d12abb9c3fdd9df6854655
akin <akin@DESKTOP-4KV3ORN>
2023-05-04 09:47:55 +0300

    Add project files.

---
+++ FullaDemirbas/Controllers/Context/MvcDemirbasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using FullaDemirbas.Controllers.Entities;
+
+namespace FullaDemirbas.Controllers.Context
+{
+    public class MvcDemirbasContext :DbContext
+    {
+        public MvcDemirbasContext() : base("Server=.;Database=MvcDemirbasDB;Trusted_Connection=true")
+        { } 
+        public DbSet<Personel> Personeller { get; set; }
+        public DbSet<Magaza> Magazalar { get; set; }
+        public DbSet<Demirbas> Demirbaslar { get; set; }
+
+    }
+}
---
+++ FullaDemirbas/Controllers/Entities/Personel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullaDemirbas.Controllers.Entities
+{
+    public class Personel
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        public string Tcno { get; set; }
+        public string Telefon { get; set; }
+        public int MagazaId { get; set; }
+    }
+}
