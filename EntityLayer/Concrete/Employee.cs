6a2604711484dc2724e88d928f9c09d312971c6b
akin <akin@DESKTOP-4KV3ORN>
2023-05-04 10:06:18 +0300

    EntityLayer katmanındaki propertylerin oluşturulması.

---
+++ EntityLayer/Concrete/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Employeename { get; set; }
+        public string EmployeeSurName { get; set; }
+        public string EmployeeImage { get; set; }
+        public string EmployeeMail { get; set; }
+        public string EmployeePassword { get; set; }
+    }
+}
