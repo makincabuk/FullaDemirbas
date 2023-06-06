ca16ad67633b28b27cd8808e6cfca88be34de3a6
mehmet akın çabuk <makincabuk@gmail.com>
2023-06-06 17:53:38 +0300

    Page403 sayfasının yapımı Brand yapısının oluşturulup back end kodlarının yazılması Model classının oluşuturlması Brand ve Model classının ilişkisel yapısının oluşturulması

---
+++ EntityLayer/Concrete/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Brand
+    {
+        [Key]
+        public int BrandID { get; set; }
+        [StringLength(100)]
+        public string BrandName { get; set; }
+
+        public ICollection<Model> model { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Model
+    {
+        [Key]
+        public int ModelID { get; set; }
+        [StringLength(100)]
+        public string ModelName { get; set; }
+
+
+        public int BrandId { get; set; }
+        public virtual Model model { get; set; }
+    }
+}
