5dcb31af0cf24409f6fce04c8a0aa80d71f56cf3
mehmet akın çabuk <makincabuk@gmail.com>
2023-06-09 12:16:51 +0300

    Admin mimarilerinin oluşuturlması. Login sayfasında doğrulama işlemlerinin yapılıp yönlendirilmesi

--- FullaDemirbas/Controllers/AdminCategoryController.cs
+++ FullaDemirbas/Controllers/AdminCategoryController.cs
@@ -15,6 +15,7 @@ namespace FullaDemirbas.Controllers
     {
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
 
+        [Authorize]
         public ActionResult Index()
         {
             var categoryvalues = cm.GetList();
--- FullaDemirbas/Controllers/LoginController.cs
+++ FullaDemirbas/Controllers/LoginController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,39 @@ namespace FullaDemirbas.Controllers
 {
     public class LoginController : Controller
     {
+        AdminManeger ADM = new AdminManeger(new EfAdminDal());
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Admin A)
+        {
+            Context c = new Context();
+            var adminuserinfo = c.Admins.FirstOrDefault(x => x.AdminUsername == A.AdminUsername && x.AdminUserPassword == A.AdminUserPassword);
+            if (adminuserinfo != null)
+            {
+
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin A)
+        {
+            ADM.AdminAdd(A);
+            return RedirectToAction("Index");
+        }
+
     }
 }
